0e2e1e0d29cc80eded128a381486a4373a4058d2
Azahel <azahelcaballero@gmail.com>
2020-09-22 16:54:28 -0600

    v

---
+++ variables.js
@@ -0,0 +1,17 @@
+var nombre  = "Azahel";
+
+//declarar 
+var edad ;
+//inicializar 
+edad = '27'
+
+edad
+
+var elementos = ["Computadora, Celular"];
+
+var persona = {
+    nombre: "Azahel",
+    edad: "27"
+}
+
+persona;
