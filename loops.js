bc4df19ceb983b4bac30a1e199f27ac040e40e30
Azahel <azahelcaballero@gmail.com>
2020-09-23 14:53:55 -0600

    for and while

---
+++ loops.js
@@ -0,0 +1,11 @@
+var estudiantes = ["aza","eve","gil","freddy"];
+
+function saludarEstudiantes(estudiantes) {
+    console.log('saluods '+estudiante);
+    
+}
+
+for (let i = 0; i < estudiantes.length; i++) {
+    const saludarEstudiantes = estudiantes[i];
+    
+}
